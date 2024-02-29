6b7813ee23b4e8d1c06833535380c9f5f241a236
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2024-02-29 16:44:53 +0700

    add style

--- src/main/java/com/Killian/crawler/controller/CrawlController.java
+++ src/main/java/com/Killian/crawler/controller/CrawlController.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
